8d40d4192a1362b2c75dcd0acefc2fae0da2326c
Aluno <aluno@imac-de-suporte-36.local>
2019-03-23 12:45:39 -0300

    Atualizando Maps

--- MyPhomeApp/VCTerceira.swift
+++ MyPhomeApp/VCTerceira.swift
@@ -9,14 +9,83 @@
 import UIKit
 import MapKit
 
-class VCTerceira: UIViewController {
+class VCTerceira:
+    UIViewController,
+    MKMapViewDelegate,
+    UIGestureRecognizerDelegate
+{
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
 
+        let ponto = gestureRecognizer.location(in: vrMap)
+        
+        let coordenadas = vrMap.convert(ponto, toCoordinateFrom: vrMap)
+        
+        let pino = MKPointAnnotation()
+        pino.title = "nova anotação"
+        pino.coordinate = coordenadas
+        vrMap.addAnnotation(pino)
+        
+        print("Um long press ocorreu")
+        
+        return true
+    }
+
+    @IBOutlet weak var vrMap: MKMapView!
+    
+    @IBAction func trataEventoSegmented(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0)
+        {
+            vrMap.mapType = .standard
+        }
+        else {
+            if(sender.selectedSegmentIndex == 1)
+            {
+                vrMap.mapType = .satellite
+            }
+            else
+            {
+                vrMap.mapType = .hybrid
+            }
+        }
+    }
+    
+    @IBAction func handleCall(_ sender: UIButton) {
+//        if let url = URL(string: "tel//\()"
+    }
+    
+    func criaPino()
+    {
+        let pino = MKPointAnnotation()
+        pino.title = "UFG Mobile"
+        
+        let coordenadas = CLLocationCoordinate2D(latitude: -16.603314, longitude: -49.267128)
+        pino.coordinate = coordenadas
+        
+        vrMap.addAnnotation(pino)
+     
+        let zoom = MKCoordinateSpanMake(0.002, 0.002)
+        let regiao = MKCoordinateRegion( center: pino.coordinate, span: zoom)
+        
+        vrMap.setRegion(regiao, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        vrMap.delegate = self
 
         // Do any additional setup after loading the view.
     }
 
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("Um pino foi selecionado no MAP")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        criaPino()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
